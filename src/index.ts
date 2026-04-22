ba3d51e44d6e6a152638474546356cdd976f808a
mohamedaflah <mohamedaflah186@gmail.com>
2026-04-22 18:31:46 +0530

    feat: secured app by using helmet

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,11 @@ import "./db/db.config";
 import { connectRedis } from "./cache/redis.config";
 import searchRouter from "./routers/search_route";
 import { errorMiddleware } from "./middlewares/error.middleware";
+import helmet from "helmet";
 const app = express();
 const port = process.env.PORT;
 connectRedis();
-
+app.use(helmet());
 app.use(`/api/search`,searchRouter)
 app.use(errorMiddleware)
 app.listen(port, () => {
